a34aeee044a2dce18f716bc294c0ef52dfcd476b
anhy167 <ttka1607@gmail.com>
2023-03-15 23:06:46 +0700

    Update login, sign up, study route

---
+++ frontend/src/app/login-form/login-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent {
+
+  form = new FormGroup({
+    'account': new FormGroup({
+      'username': new FormControl('',
+      Validators.required,
+      UsernameValidators.shouldBeUnique),
+      'password': new FormControl('',Validators.required),
+    }),
+  })
+
+  get username(){
+    return this.form.get('account.username');
+  }
+}
